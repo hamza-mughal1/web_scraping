9d4ec7e613ec62d11278a46f598098290a56d758
hamza_mughal <96692951+hamza-mughal1@users.noreply.github.com>
2023-10-24 22:04:38 +0500

    Update daraz_extraction.py

--- daraz_extraction.py
+++ daraz_extraction.py
@@ -1,19 +1,103 @@
-import requests 
-from bs4 import BeautifulSoup
-
-session = requests.Session()
-
-
-str = "t_fv=1698037523280; t_uid=SskEfQZbFN4BfmopMXEv0PWYd3Pqff82; lzd_cid=39f0f04c-adb6-4ea3-b4a1-22f3284b6809; lzd_sid=138c3f2fc9ac4fa1ec9a6db532903966; _tb_token_=eebd7d366333e; lwrid=AQGLWuxnDfU1hOT9KoVDX380peG0; _gcl_au=1.1.1140994701.1698037525; _gid=GA1.2.1058417882.1698037526; hng=PK|en-PK|PKR|586; curTraffic=lazada; userLanguageML=en-PK; _bl_uid=j8l6dowy2g0ftIpIafU2t7g7zpp9; mi_p_source=undefined; mi_p_medium=undefined; mi_p_campaign=undefined; mi_p_term=undefined; mi_p_content=undefined; mi_p_gclid=undefined; t_sid=dCJ5GrxAilF9iOB832MiYbFn1CLXp8SP; utm_channel=NA; _m_h5_tk=343e773e7552de2c5a4d97a91a1c022b_1698055814415; _m_h5_tk_enc=d7dc8bd59dea93c891b016000ed5fe60; daraz-marketing-tracker=hide; epssw=1*MZP911GPC5HtTFzaIA4SZRwdu5b8Jj9p1Z2sNADGBNi3oGeGiFrstQJ_xH-MjhxGcOYX3cBWNKB9-HWDdt5Rt962NKdM1u9Vd1XxQxe5NCsGj_9f6TO0av9Vqbuc9J-h4_X2AT0cFb2ZdaKNV6CawvOAlz6jxAv8dL3bvp6-xD0RyTBqdGWJ3Jp8fgnnxYB4yaQRrW_jnK5b6by9BxzRPMmnetrnxf..; JSESSIONID=D6CDDF253987E905D1BF3CA645F29F1C; _ga_5L4FRV3KPW=GS1.1.1698046813.2.1.1698046821.52.0.0; _ga_C6SBBPVWWK=GS1.1.1698046813.2.1.1698046821.52.0.0; _ga=GA1.2.466404133.1698037526; tfstk=dg-9FRA6OXcMXoiNHr3ngXv9qLXt-hpwjCJ7msfglBdpFLPGljuNlipOtsjGffSAhIACsCj6DervUK-iQiDNbdoVVbcoq0XwQiSfvFnoqnPm_rloZ0murSX_1bYGhfFAUi2uZcFL23KPCqvFmC5gkhWOW_UG9oExnOQONd1KNb6w33x8TCFlwt4spvU4uN6eTxC0s; l=fBx0m16PP_zAVTDzBOfwPurza77OSKRAguPzaNbMi9fPOX5p53kCW13rFhY9C3MNFsTyR3ohWZKMBeYBqIXN3PYgulPFYMkmnXr9aX5..; isg=BI2N2eNK1FqYKXB1wNekuHwmjKkHasE8GQm62M8S7CSTxq94l7tgDFtENkKgBtn0"
-
-dic = {}
-for i in str.split(";"):
-    key ,val = i.split("=")
-    dic[key] = val
-
-session.cookies = dic
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import pandas as pd
+import time
 
 
-requests_cookies = requests.utils.cookiejar_from_dict(dic)
-response = session.get("https://www.daraz.pk/catalog/?spm=a2a0e.home.search.1.35e34076ISp6pa&q=gaming%20pc&_keyori=ss&from=search_history&sugg=gaming%20pc_0_1", cookies=requests_cookies)
-print(response)
+
+def func(name,function,element):
+        try:
+            if function == 0:    
+                result = name.find_element(*element)
+                return result
+            elif function == 1:
+                result = name.find_elements(*element)
+                return result
+        except:
+             return None
+
+
+
+
+
+
+
+
+
+
+url = "https://www.daraz.pk/catalog/?spm=a2a0e.home.search.1.35e340761IDnMC&q=gaming%20pc&_keyori=ss&from=search_history&sugg=gaming%20pc_0_1"
+
+option = webdriver.ChromeOptions()
+option.add_argument('--headless')
+
+
+s = Service("C:/Users/Hamza/Documents/web_scraping/chromedriver.exe")
+driver = webdriver.Chrome(service= s)
+driver.get(url)
+
+
+d = func(driver,1,(By.CSS_SELECTOR, "div[class='gridItem--Yd0sa'][data-qa-locator='product-item'][data-tracking='product-card']"))
+
+
+title = []
+price = []
+discount = []
+stars = []
+rating_no = []
+link = []
+for i in d:
+    lis = []
+    tx = func(i,0,(By.CSS_SELECTOR,"div[class='title--wFj93']"))
+    if tx == None:
+        title.append(None)  
+    else:   
+        title.append(tx.text)
+    a = func(tx,0,(By.CSS_SELECTOR,"a"))
+    if a == None:
+        link.append(None)
+    else:
+        link.append(a.get_attribute("href"))
+    px = func(i,0,(By.CSS_SELECTOR,"div[class='price--NVB62']"))
+    if px == None:
+        price.append(None)
+    else:
+        price.append(px.text)
+
+    dx = func(i,0,(By.CSS_SELECTOR,"div[class='priceExtra--ocAYk']"))
+    dx2 = func(dx,0,(By.CSS_SELECTOR,"span[class='origPrice--AJxRs']"))
+    dx3 = func(dx2,0,(By.CSS_SELECTOR,"del[class='currency--GVKjl']"))
+    if dx3 == None:
+        lis.append(None)
+    else:
+        lis.append(dx3.text)
+    dx4 = func(dx,0,(By.CSS_SELECTOR,"span[class='discount--HADrg']"))
+    if dx4 == None:
+        lis.append(None)
+    else:
+        lis.append(dx4.text)
+    discount.append(lis)
+
+    rx = func(i,0,(By.CSS_SELECTOR,"div[class='rateAndLoc--XWchq']"))
+    rx2 = func(rx,0,(By.CSS_SELECTOR,"div[class='rating--ZI3Ol rate--DCc4j']"))
+    rx3 = func(rx2,1,(By.CSS_SELECTOR,"span"))
+    rx4 = func(rx2,0,(By.CSS_SELECTOR,"span[class='rating__review--ygkUy']"))
+    if rx4 == None:
+        rating_no.append(None)
+    else:
+        rating_no.append(rx4.text)
+    if rx3 == None:
+        stars.append(None)
+    else:
+        stars.append(len(rx3)-1)
+
+
+df = pd.DataFrame({"Title":title,
+                   "price":price,
+                   "discount":discount,
+                   "stars":stars,
+                   "rating numbers":rating_no,
+                   "link":link})
+
+
+df.to_excel("daraz_data.xlsx")
