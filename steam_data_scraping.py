f9a65c15cdc28eb672898e14e2759c7d9e3ec825
hamza_mughal <96692951+hamza-mughal1@users.noreply.github.com>
2023-10-27 16:50:57 +0500

    Update steam_data_scraping.py

--- steam_data_scraping.py
+++ steam_data_scraping.py
@@ -1,14 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
 
+def selenium_to_load_full_page(url):
+    option = webdriver.ChromeOptions()
+    option.add_argument('--headless')
 
+    s = Service("C:/Users/Hamza/Documents/web_scraping/chromedriver.exe")
+    driver = webdriver.Chrome(service= s,options=option)
+    driver.get(url)
+    older = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        time.sleep(1)
+        old_height = driver.execute_script("return document.body.scrollHeight")
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if (old_height == new_height) and (older == new_height):
+            return driver.page_source
+        older = old_height
+        
 def list_of_products(url):
     lis = []
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text,"lxml")
+    response = selenium_to_load_full_page(url)
+    soup = BeautifulSoup(response,"lxml")
     div = soup.find("div",id="search_resultsRows")
     products = div.find_all("a",class_ = "search_result_row ds_collapse_flag")
+    print("lenght = ",len(products))
 
     for i in products:
         lis.append(i.get("href"))
@@ -70,7 +90,8 @@ def extract_data_of_game(product_url):
 def main(url,titles,genre,developers,publishers,franchises,release_dates,websites,twitter,youtube,links):
     products = list_of_products(url)
 
-    for i in products:
+    for pos,i in enumerate(products):
+        print(pos)
         extract = extract_data_of_game(i)
         titles.append(extract[0])
         genre.append(extract[1])
@@ -85,7 +106,7 @@ def main(url,titles,genre,developers,publishers,franchises,release_dates,website
     links[:] = products
 
 if __name__ == "__main__":
-    url = "https://store.steampowered.com/search/?filter=popularcomingsoon&os=win"
+    url = "https://store.steampowered.com/search/?filter=popularcomingsoon&ndl=1"
     titles = []
     genre = []
     developers = []
@@ -112,4 +133,5 @@ if __name__ == "__main__":
                        "links":links})
     
     df.to_excel("steam_upcoming_games.xlsx")
+
     
