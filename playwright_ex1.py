70c026fa98909e5dfb6c985c30df5c279ca5857c
hamza_mughal <96692951+hamza-mughal1@users.noreply.github.com>
2024-01-03 23:17:51 +0500

    87

--- playwright_ex1.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import sync_playwright
-
-with sync_playwright() as playwright:
-
-    browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto("https://www.youtube.com")
-
-    browser.close()
